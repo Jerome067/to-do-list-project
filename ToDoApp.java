59c82bc1502fb16f870aebb0c46c31383f11a832
Jerome067 <delrosariomrj@gmail.com>
2025-09-22 17:31:27 +0800

    Improvements in the UI of ToDoApp.java

--- ToDoApp.java
+++ ToDoApp.java
@@ -1,5 +1,4 @@
 // ToDoApp.java
-// Edit test
 import java.util.*;
 
 public class ToDoApp {
@@ -8,12 +7,12 @@ public class ToDoApp {
 
     public static void addtask(String t) {
         tasks.add(t);
-        System.out.println("Task Added!!");
+        System.out.println(" Task is Added!!");
     }
 
     public static void showTasks() {
         if (tasks.size() == 0) {
-            System.out.println("No tasks yet");
+            System.out.println(" No Task/s yet!!");
         } else {
             for (int i = 0; i <= tasks.size(); i++) {
                 System.out.println((i + 1) + ". " + tasks.get(i));
@@ -23,40 +22,62 @@ public class ToDoApp {
 
     public static void removeTask(int n) {
         tasks.remove(n);
-        System.out.println("task removed");
+        System.out.println(" Task is Remove!!");
     }
 
     public static void main(String args[]) {
         Scanner s = new Scanner(System.in);
         while (true) {
-            System.out.println("=============================");
-            System.out.println("      TO DO LIST (JAVA)");
-            System.out.println("=============================");
-            System.out.println("[1] Add Task");
-            System.out.println("[2] Show Tasks");
-            System.out.println("[3] Remove Task");
-            System.out.println("[4] Exit");
-            System.out.println("=============================");
-            System.out.println("Please enter your choice: ");
+            System.out.println("=================================");
+            System.out.println("        TO DO LIST (JAVA)");
+            System.out.println("=================================");
+            System.out.println("  [1] Add Task");
+            System.out.println("  [2] Show Tasks");
+            System.out.println("  [3] Remove Task");
+            System.out.println("  [4] Exit");
+            System.out.println("=================================");
+            System.out.print(" Please enter your choice: ");
             int choice = s.nextInt();
-            System.out.println("=============================");
+            System.out.println("=================================");
+            
             if (choice == 1) {
-                System.out.println("Enter Task: ");
+                System.out.println("=================================");
+                System.out.println("   [1] Add Task");
+                System.out.println("=================================");
+                System.out.print(" Enter Task Name: ");
                 String t = s.nextLine();
                 addtask(t);
+                System.out.println("=================================");
+                
             } else if (choice == 2) {
+                System.out.println("=================================");
+                System.out.println("   [2] Show Task");
+                System.out.println("=================================");
                 showTasks();
+                System.out.println("=================================");
+                
             } else if (choice == 3) {
+                System.out.println("=================================");
+                System.out.println("   [3] Remove Task");
+                System.out.println("=================================");
                 System.out.print("Enter task no to remove: ");
                 int n = s.nextInt();
-                removeTask(n); // <--
+                removeTask(n);
+                System.out.println("=================================");
+
             } else if (choice == 4) {
+                System.out.println("=================================");
+                System.out.println("   [4] Exit");
+                System.out.println("=================================\n");
+                System.out.println(" Exiting the program,\n have a nice day!\n");
+                System.out.println("=================================");
                 break;
             } else {
-                System.out.println("Wrong Choice!!");
+                System.out.println("=================================");
+                System.out.println(" Wrong choice, please try again!\n");
+                System.out.println("=================================");
             }
         }
     }
 }
-
 
