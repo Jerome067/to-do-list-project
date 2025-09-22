09842253c69209b57b803dcf1fcc4e8b4eeecf7f
pinzap_ <151607984+judSoftwareDev@users.noreply.github.com>
2025-09-22 17:12:55 +0800

    Fix spaces and word conventions

--- toDoApp.py
+++ toDoApp.py
@@ -1,39 +1,43 @@
 # toDoApp.py
 
-tasks=[]
+tasks = []
 
-def addtask(task) :
-  tasks.append(task)
-  print("task added!")
+def addtask(task): # <--- alligned this colon with def
+    tasks.append(task)
+    print("\nTask added!") 
 
-def showTasks( ):
-    if len(tasks)==0 :
-      print("no tasks yet")
+def showTasks():
+    if len(tasks) == 0:
+        print("No tasks yet")
     else:
-     for i in range (len(tasks)):
-      print(i+1,".",tasks[i])
+        for i in range(len(tasks)):
+            print(i+1, ".", tasks[i])
 
 def removetask(tasknumber):
-    tasks.pop(tasknumber) 
-    print("task removed!!")
+    tasks.pop(tasknumber)
+    print("Task removed!!")
 
 def main():
     while True:
-        print("1 Add Task")
-        print("2.Show Tasks")
-        print("3.Remove Task")
-        print("4- Exit")
-        ch = input("enter choice : ")
-        if ch=="1":
-            t = input("enter task : ")
+        print("1. Add Task")
+        print("2. Show Tasks")
+        print("3. Remove Task")
+        print("4. Exit")
+        ch = input("Enter choice: ")
+        if ch == "1":
+            t = input("Enter task: ")
             addtask(t)
-        elif ch=="2":
+        elif ch == "2":
             showTasks()
-        elif ch=="3":
-            n=int(input("enter task no to remove: "))
-            removetask(n)   
-        elif ch=="4":
-            break;
+        elif ch == "3":
+            n = int(input("Enter task no to remove: "))
+            if 1 <= n <= len(tasks):
+                removetask(n-1)
+            else:
+                print("Invalid task number!")
+        elif ch == "4":
+            break
         else:
-            print("wrong choice!!")
+            print("\nWrong choice!")
+
 main()
